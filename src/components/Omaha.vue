9e8ce1c0798d0f421851fa782962238b6f41e3f7
Jarrod <jarrod@digital-efficiency.co.nz>
2017-12-18 20:01:59 +1300

    First Commit

---
+++ src/components/Omaha.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <img src="http://www.lifesaving.org.nz/media/134123/slsnr_logov3.jpg" alt="">
+      <h1>Be safe in the sun this summer - Surf Life Saving Northern Region</h1>
+    </header>
+    <div class="hero"></div>
+    <div class="beacon">
+      <h2>Todays Conditions</h2>
+      <div class="uvindex">
+        UV Index:
+        9 - Very High
+      </div>
+      <p>Remember to apply sunblock regularly</p>
+      <div class="waves"><svg width="90" height="17" viewBox="0 0 90 17" xmlns="http://www.w3.org/2000/svg"><title>Artboard</title><g fill="none" fill-rule="evenodd"><g fill-rule="nonzero"><path fill="#000" d="M.5 0H14v10H1v7H0V0"/><path fill="#E31F26" d="M1 1h12v4H1"/><path fill="#FFD900" d="M1 5h12v4H1"/></g><g fill-rule="nonzero"><path fill="#000" d="M76.5 0H90v10H77v7h-1V0"/><path fill="#E31F26" d="M77 1h12v4H77"/><path fill="#FFD900" d="M77 5h12v4H77"/></g><path d="M22 5s3.235 6 11 6c7.765 0 12-6 12-6s4 6 11 6 12-6 12-6" stroke="#006BB6" stroke-width="2" stroke-linecap="square"/></g></svg></div>
+		<p>Only swim between the flags</p>
+    </div>
+  </div>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAQASXDYBS3PYUOrvVP-vtXsIYHCuIK3yw",
+  authDomain: "keystorage-70531.firebaseapp.com",
+  databaseURL: "https://keystorage-70531.firebaseio.com",
+  projectId: "keystorage-70531",
+  storageBucket: "keystorage-70531.appspot.com",
+  messagingSenderId: "437893084856"
+}
